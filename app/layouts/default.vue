8d9e39929e1b622bcd3c622c58e70909bc0591df
nemanjadragun <nemanjadragun92@gmail.com>
2025-09-30 20:02:52 +0200

    Enhance header with navigation controls and route metadata display
    - Added next/previous route navigation buttons with click handlers.
    - Displayed current route name and index in the header.
    - Updated styles for improved layout consistency.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,30 @@
 <template>
   <header
-    class="border-b-2 bg-neutral-900 border-b-pink-800 h-16 sticky top-0 z-20 flex items-center px-4"
+    class="border-b-2 bg-neutral-900 border-b-pink-800 h-16 sticky top-0 z-20 flex justify-between items-center px-4 text-white"
   >
     <div class="flex items-center gap-2">
       <UiLogo />
       <sup class="font-semibold text-pink-800">Workshop</sup>
+    </div>
+    <div class="font-semibold flex items-center gap-2">
+      <button
+        class="size-6 flex items-center justify-center bg-neutral-800 border border-pink-800 rounded hover:bg-neutral-700"
+        @click="navigateToPreviousRoute"
+      >
+        <Icon name="mdi:chevron-left" />
+      </button>
+      <h2>
+        {{ returnCurrentRoute?.meta?.displayName || "Missing displayName" }}
+      </h2>
+      <button
+        class="size-6 flex items-center justify-center bg-neutral-800 border border-pink-800 rounded hover:bg-neutral-700"
+        @click="navigateToNextRoute"
+      >
+        <Icon name="mdi:chevron-right" />
+      </button>
+    </div>
+    <div class="text-sm">
+      {{ returnCurrentRouteIndex + 1 }} of {{ routes.length }}
     </div>
   </header>
   <div class="flex flex-1 min-h-0">
@@ -40,4 +60,32 @@ const router = useRouter();
 const routes = computed(() => router.options.routes);
 
 console.log("routes", routes.value);
+
+const route = useRoute();
+
+const routeName = computed(() => route.name);
+
+const returnCurrentRoute = computed(() => {
+  return routes.value.find((route) => route.name === routeName.value);
+});
+
+const returnCurrentRouteIndex = computed(() => {
+  return routes.value.findIndex((route) => route.name === routeName.value) ?? 0;
+});
+
+const navigateToPreviousRoute = () => {
+  const previousRouteIndex = returnCurrentRouteIndex.value - 1;
+  const previousRoute = routes.value[previousRouteIndex];
+  if (previousRoute) {
+    router.push({ name: previousRoute.name });
+  }
+};
+
+const navigateToNextRoute = () => {
+  const nextRouteIndex = returnCurrentRouteIndex.value + 1;
+  const nextRoute = routes.value[nextRouteIndex];
+  if (nextRoute) {
+    router.push({ name: nextRoute.name });
+  }
+};
 </script>
